dab63d3452b226bfcefdda38a087fa4393569d63
Yanis Rezgui <yanisrezgui28@gmail.com>
2024-10-18 17:47:49 +0100

    Add files via upload

--- calculator.c
+++ calculator.c
@@ -1,65 +1,60 @@
 #include <stdio.h>
- main(){
-
-    double num1;
-    double num2;
-    double result;
-    char operator;
-    
-    printf("choose an operator (+, -, *, /) : ");
-    scanf(" %c", &operator);
-    
-    printf("Enter number 1 :");
-    scanf("%lf", &num1);
-    
-    printf("Enter number 2 :");
-    scanf("%lf", &num2);
-    
-    switch(operator){
-    	case '+':
-    		result = num1 + num2;
-    		printf("result is : %lf", result);
-    		
-    		break;
-    		
-    	case '-' :
-    		result = num1 - num2;
-    		printf("The result is : %lf", result);
-    		
-    		break;
-    		
-    	case '*':
-    		result = num1 * num2;
-    		printf("The result is : %lf", result);
-    		
-    		break;
-    		
-    	case '/':
-    		if(num1 == 0 || num2 == 0){
-    			printf("Math error impossible to devide by 0");
-    			
+main(){
+	double num1;
+	double num2;
+	double result ;
+	char operator;
+	
+	printf("\nChoose an operator(+,-,/,*) : ");
+	scanf("%c", &operator);
+	printf("\nEnter number 1 : ");
+	scanf("%lf", &num1);
+	
+	printf("\nEnter nuber 2 : ");
+	scanf("%lf", &num2);
+	
+	switch(operator){
+		case '+':
+			result = num1 + num2;
+			printf("The result is : %.2lf\n", result);
+			
+			break;
+			
+		case '-':
+			result = num1 - num2;
+			printf("The result is : %.2lf\n", result);
+			break;
+			
+		case '*':
+			result = num1*num2;
+			printf("The result is : %.2lf\n", result );
+			
+			break;
+			
+		case '/':
+			if(num1 == 0 || num2 == 0){
+				printf("Math error impossible to devide by zero \n");
 			}else if(num1 != 0 && num2 != 0){
-				result = num1 / num2;
-				printf("The result is : %lf", result);
+				result = num1/num2;
+				printf("The result is : %.2lf\n", result);
 			}
 			
 			break;
 			
-		default :
-			printf("Math error : please enter correct operator");
-			
+		default:
+			printf("Math error : please verify that you entered the correct operator\n");
 			break;
+			
+			
+			
 	}
 	
-	printf("\nEnd of switch statement");
-	
-    
-    
-    
-   
-    
-    return 0;
-    
+	printf("************************************\n");
 	
 	
+	printf("End of switch statement :) \n");
 }
+
+
+
+
--- mounths.c
+++ mounths.c
@@ -1,33 +1,23 @@
 #include <stdio.h>
-/// @brief 
-/// @return 
-int main()
-{
-    enum months {jan = 1,feb = 2,mar = 3,apr = 4,may = 5,jun = 6,jul = 7,aou = 8,sep = 9,oct = 10,nov = 11,dec = 12 };
-    int month;
-    printf("Enter the number of the month :\n");
-    scanf("%d", &month);
 
-    if((month == 12) || ( month == 1) || (month == 2))
-    {
-        printf("Its winter , its very cold outside !!\n");
-    }else if((month == 3) || (month == 4) || (month == 5 ))
-          {
-            printf("Its spring !! the flowers are beautiful\n");
-          }else if((month == 6) || (month == 7) || (month == 8))
-         { 
-            printf("Its summer let's go to the beach !!\n");
-         }else if((month == 9) || (month == 10) || (month == 11))
-         {
-            printf("Its autumn !!, we gotta get back to school :(\n");
-         }else{
-                 printf("Error :Invalid month\n");
-              }
-
-
-     return 0;
-             
-         
-
-
+main(){
+	enum mounths { Jan = 1, Feb = 2, Mar = 3, Apr= 4, May = 5, Jun = 6, Jul =7, Aug = 8, Sep = 9, Oct = 10, Nov = 11, Dec = 12 
+	};
+	int mounth;
+	printf("\nEnter the number of your mounth : ");
+	scanf("%d", &mounth);
+	if(mounth == 1 || mounth == 2 || mounth == 12){
+		printf("It\'s Winter , it\'s cold outside :(\n");
+	}else if(mounth == 3 || mounth == 4 || mounth == 5){
+		printf("It\'s spring ! The trees are butifull and the flowers are gorgeous :)\n");
+	}else if(mounth == 6 || mounth == 7 || mounth == 8){
+		printf("Youha! it\'s summer let's go to the beach :)\n");
+	}else if(mounth == 9 || mounth == 10 || mounth == 11){
+		printf("It\'s authumn, It\'s butifull outside :)\n");
+	}else{
+		printf("Wrong number, check again ;)");
+	}
+	
+	
+	
 }
