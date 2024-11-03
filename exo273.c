99e8fc8810beae7b2c6a154de8cd9fdbe67be3e7
Yanis Rezgui <yanisrezgui28@gmail.com>
2024-11-03 23:15:52 +0100

    Add files via upload

--- exo273.c
+++ exo273.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
-#include <math.h>
 
 int main(){
 
     int n;
-    float i, S;
+    float S,i;
 
-    printf("\nVeillez entrer une valeur pour n : ");
+    printf("\nEnter a value for n : ");
     scanf("%d", &n);
 
     S = 0;
-
-    for(i = 0; i <= n; i++){
+    for(i = 0;i <= n; i++){
 
         S = S + pow(10, i);
     }
 
-    printf("\nLa valeurr de la somme est : %.2f", S);
-
-
-
+    printf("\nThe final value of n powers of 10 is : %.2f", S);
 
     return 0;
-
 }
