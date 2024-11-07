e6683db75ce1fe19b2ece76519b68749b6e1e244
Yanis Rezgui <yanisrezgui28@gmail.com>
2024-11-07 23:20:13 +0100

    Add files via upload

--- exo5.c
+++ exo5.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
-main(){
-	
-	float s,p,r;
-	float pi = 3.14;
-	
-	printf("\nEnter a value for r : ");
-	scanf("%f", &r);
-	
-	s = 2*pi*pow(r, 2);
-	
-	p = 2*pi*r;
-	
-	printf("\nThe Surface is : %.2f, \nThe sirc is : %.2f", s,p);
-}
+int main(){
+
+    int lines, i,r;
+
+
+    printf("\nEnter a number of lines that must be of course positive : ");
+    scanf("%d", &lines);
+
+    for(i = 1; i <= lines; i++){
+        for(r = 1;r<=i;r++){
+
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+     
+    }
--- exo6.c
+++ exo6.c
@@ -1,19 +1,47 @@
 #include <stdio.h>
 
-main(){
-	
-	int x,y,z;
-	
-	printf("\nEnter x and y : ");
-	scanf("%d%d", &x, &y);
-	
-	printf("\nThe values of before switching : x = %d, y = %d", x, y);
-	
-	z = x;
-	x = y;
-	y = z;
-	
-	printf("\nThe values after switching are : x = %d, y = %d", x, y);
-	
-	
+int main(){
+
+    int i,s,n;
+/*
+    s = 0;
+    for(i = 0; i<4;i++){
+
+        printf("\nENter an integer number : ");
+        scanf("%d", &n);
+
+         s += n;
+    }
+
+    printf("\nThe sum is : %d", s);
+    */
+
+  /*s = 0;
+   i = 0;
+   while(i < 4){
+      printf("\nEnter the value of n : ");
+      scanf("%d", &n);
+
+      s += n;
+      i++;
+   }
+
+   //printf("\nThe sum is : %d", s);
+
+   */
+
+  s = 0;
+  i = 0;
+
+  do{
+    printf("\nEnter a positive integer n : ");
+    scanf("%d", &n);
+
+    s += n;
+    i++;
+  }while(i < 4);
+
+  printf("\nThe sum is : %d", s);
+
+    return 0;
 }
