89b65163a5f88387e4eca46ebd00b778bfd35487
Yanis Rezgui <yanisrezgui28@gmail.com>
2024-11-12 23:21:15 +0100

    Add files via upload

--- sum.c
+++ sum.c
@@ -1,59 +1,20 @@
 #include <stdio.h>
- 
- int main(){
-   
-   double num1;
-   double num2;
-   double result;
-   char operator;
 
-   printf("Pick an operator ( + , - , * , /) :\n");
-   scanf("%c", &operator);
+int main(){
 
-   printf("Enter the value of number 1 :\n");
-   scanf("%lf", &num1);
+    int i,n,S,E;
 
-   printf("Enter the value of number 2 :\n ");
-   scanf("%lf", &num2);
+    printf("\nEnter a value for n : ");
+    scanf("%d", &n);
 
-   switch (operator){
-    case  '+':
-      result = num1 + num2;
-      printf("\nThe result is : %lf", result);
-      break;
+    S = 1;
+    E = 1;
 
-    case '-':
-      result = num1 - num2;
-      printf("\nTHe result is : %lf", result);
-      break;
+    for(i = 2; i<=n;i++){
 
-    case '*':
-      result = num1 * num2;
-      printf("\nTHe result is : %lf", result);
-
-      break;
-
-
-    case '/':
-    if(num1 != 0 && num2 != 0){
-        result = num1 / num2;
-        printf("\nThe result is : %lf", result );
-
-    }else{
-        printf("\nError , impossible to devide by 0");
+        S = S + i;
+        E = E*S;
     }
 
-    break;
-
-    default :
-       printf("\nError : invalid operator, please choose one of the four operator presented ");
-
-   }
-
-   printf("End of switch statement");
-
-
-   return 0;
-
-
- }
+    printf("E = %d", E);
+}
