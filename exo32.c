c79a7c16a9c99fe7a513268f95e629e7c48201a0
Yanis Rezgui <yanisrezgui28@gmail.com>
2024-11-21 22:46:17 +0100

    Add files via upload

--- exo32.c
+++ exo32.c
@@ -1,21 +1,59 @@
 #include <stdio.h>
 
 int main(){
-     float m,a, ans;
-     int n;
 
-     m = 1000000;
-     a = 500000;
-     ans = 0;
-      while(a<m){
-         
-         m += 50000;
-         a = a + (a*8)/100;
-         ans++;
-      }
+    int S,n,min,max,cpt,op;
+    float moy;
 
-      printf("\nLe nombre d'annes necessaires pour que les nombres d'habitant d'agaqdir depasse celui de marakech est : %.1f ans", ans);
+    printf("\nEnter a number n : ");
+    scanf("%d", &n);
 
-      return 0;
+    min = n;
+    max = n;
+    S = 0;
+    cpt = 0;
+    moy = 0;
 
+    while(n != 0){
+
+        S = S + n;
+        cpt++;
+        
+        if(n<min){
+            min = n;
+        }else if(max<n){
+            max = n;
+        }
+        printf("\nEnter a number n : ");
+        scanf("%d", &n);
+
+    }
+    moy = S/cpt;
+    
+    do{
+        printf("\nEntez 1 pour afficher le minimum : ");
+        printf("\nEntrez 2 pour afficher le max : " );
+        printf("\nEntrez 3 pour afficher la moyenne : ");
+        scanf("%d", &op);
+    }while(op == 1 && op == 2 && op == 3);
+    
+    switch(op){
+        case 1 : printf("\nThe minimum is : %d", min);
+        break;
+
+        case 2 : printf("\nThe maximum is : %d ", max);
+        break;
+
+        case 3 : printf("\nLa moyenne est : %.2f", moy);
+        break;
+
+        default : 
+           printf("\nErreur !!");
+           break;
+
+    }
+
+    return 0;
+
+    
 }
