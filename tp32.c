0e64bcfb853fbc7a8c34468c8f1639b58b4ac290
Yanis Rezgui <yanisrezgui28@gmail.com>
2024-12-04 23:22:04 +0100

    Add files via upload

--- tp32.c
+++ tp32.c
@@ -1,89 +1,78 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 
 int exo1(){
 
     int i,N;
-    do{
-        printf("\nVEillez entrez le taille du tableau : ");
-        scanf("%d", &N);
-    }while(N<0);
+
+    printf("\nEnter the size of the array : ");
+    scanf("%d", &N);
 
     int tab[N];
-    tab[0] = 1;
-    for(i=1;i<N;i++){
-       tab[i]= tab[i-1]*2;
-    }
 
-    printf("\n ");
-    printf("\nLes valeurs du tableau sont : ");
+    tab[0]=1;
+
+    for(i=1;i<N;i++){
+        tab[i]=tab[i-1]*2;
+    }
 
     for(i=0;i<N;i++){
-        printf("%d | ", tab[i]);
+        printf(" %d | ", tab[i]);
     }
-
-
-    printf("\n\n");
     return 0;
-
-    
 }
 
 int exo2(){
+
     int i,N;
 
-    do{
-        printf("\nVeillez entrez la taille du tableau : ");
-        scanf("%d", &N);
-    }while(N<0);
+    printf("\nVeillez entrez la taille du tableau : ");
+    scanf("%d", &N);
 
-    int tab[N];
+    int T[N];
+     
+     for(i=0;i<N;i++){
+    printf("\nVeillez entrez la valeur de la case %d : ", i);
+    scanf("%d", &T[i]);
+     }
 
-    for(i=0;i<N;i++){
-        printf("\nVeillez entrez la valeur de la case %d : ", i);
-        scanf("%d", &tab[i]);
-    }
-
-    printf("\n");
-    for(i=0;i<N;i++){
-        if(i%2 == 0){
-            printf("%d | ", tab[i]);
+     for(i=0;i<N;i++){
+        if(i%2!=0){
+            printf(" %d | ", T[i]);
         }
-
-    }
-    return 0;
+     }
+     return 0;
 }
 
 int exo3(){
-
-    int i,N,s;
+    int i,N;
 
     do{
-        printf("\nVeillez entrez la taille du vecteur : ");
+        printf("\nVEillez entrez la taille du vecteur : ");
         scanf("%d", &N);
     }while(N<0);
 
-    s=0;
 
-    int tab[N];
+    int T[N];
+    int S =0;
 
     for(i=0;i<N;i++){
-       printf("\nVeillez saisir la valeur de la case %d : ", i);
-       scanf("%d", &tab[i]);
+        printf("\nVeillez entrez la valeur de la case  %d : ", i);
+        scanf("%d", &T[i]);
     }
 
-      printf("\n\n");
+    for(i=0;i<N;i++){
+        S += T[i];
+    }
+    printf("\n");
+    printf("\nThe sum is : %d", S);
 
-       for(i=0;i<N;i++){
-          s = s + tab[i];
-       }
 
-       printf("\nThe sum is : %d", s);
-       printf("\n*****************");
+return 0;
 
-       return 0;
 }
 
 int exo4(){
@@ -91,133 +80,167 @@ int exo4(){
     int i,N;
 
     do{
-        printf("\nVeillez entrez la taille du tableau : ");
+        printf("\nVeillez entrez l ataille du vecteur : ");
         scanf("%d", &N);
     }while(N<0);
-     printf("\n");
+
+    int T[N];
     int max;
 
-    int tab[N];
-
     for(i=0;i<N;i++){
-        printf("\nVeillez saisir la valeur de la case  %d : ", i);
-        scanf("%d", &tab[i]);
+        printf("\nVeillez entrez la valeur de la case %d : ", i);
+        scanf("%d", &T[i]);
     }
-    
-    max = tab[0];
+    max = T[0];
 
     for(i=0;i<N;i++){
-        if(max<tab[i]){
-            max = tab[i];
+        if(max<T[i]){
+            max = T[i];
         }
     }
-    printf("\n");
 
-    printf("\nLa maximum du tableau est : %d", max);
+    
+    printf("\n\nThe maximum is : %d", max);
 
     return 0;
-
 }
+
 
 int exo5(){
 
-    int i,N,m,p=0;
-do{
-    printf("\nVeillez entrez la taille du vecteur : ");
-    scanf("%d", &N);
-}while(N<0);
+    int i,N;
 
-    int tab[N];
-
-    for(i=0;i<N;i++){
-        printf("\nVeillez saisir la valeur de la case %d : ", i);
-        scanf("%d", &tab[i]);
-    }
-   printf("\n");
-    for(i=0;i<N;i++){
-        printf(" %d || ", tab[i]);
-    }
-printf("\n");
-    printf("\nVeillez entrez un entier m a rechercher : ");
-    scanf("%d", &m);
+    do{
+        printf("\nVeillez entrez l ataille du vecteur : ");
+        scanf("%d", &N);
+    }while(N<0);
 
 
-    for(i=0;i<N;i++){
-        if(m==tab[i]){
-            p = 1;
+    int T[N];
+     int p = 1;
+
+     for(i=0;i<N;i++){
+
+        printf("\nVeillez entrez la valeur de la case %d : ",i);
+        scanf("%d", &T[i]);
+     }
+     
+     printf("\n\nLe tableau est : ");
+     for(i=0;i<N;i++){
+        
+        printf(" %d |", T[i]);
+     }
+
+     int m;
+
+     printf("\n\nVeillez entrez un entier m a rechercher : ");
+     scanf("%d", &m);
+
+     for(i=0;i<N;i++){
+        if(T[i]==m){
+            p=1;
             break;
+        }else{
+            p=0;
         }
-    }
-    printf("\n");
-    if(p==1){
-        printf("\n%d existe dans le tableau", m);
-         
-    }else{
-        printf("\n%d n'existe pas dans le tableau",m);
-    }
+     }
 
-    return 0;
+     if(p==1){
+        printf("\n\nL'entier %d existe dans le tableau", m);
+     }else{
+        printf("\nL'entier %d n'existe pas dans le tableau", m);
+     }
+     
+
+     return 0;
 
 }
 
+
 int exo6(){
 
-    int i,N,j,p=0;
+    int i,N,j,val;
 
     do{
         printf("\nVeillez entrez la taille du vecteur : ");
         scanf("%d", &N);
     }while(N<0);
+  
+     int T[N];
+    for(i=0;i<N;i++){
+        printf("\nVeillez entrez la valeur de la cse %d : ", i);
+        scanf("%d", &T[i]);
+    }
 
-    int tab[N];
+    printf("\nVoici les valeurs du tableau : ");
+    for(i=0;i<N;i++){
+        printf(" %d | ", T[i]);
+    }
+
+    printf("\n\nEntrez la valeur que vous voulez supprimer du tableau : ");
+    scanf("%d", &val);
 
     for(i=0;i<N;i++){
-        printf("\nVeillez saisir la valeur %d : ", i);
-        scanf("%d", &tab[i]);
-    }
-    printf("\n");
-    printf("les valeurs du tableau sont : ");
+        if(T[i]==val){
+            for(j=i;j<N-1;j++){
+                T[j]=T[j+1];
+            }
+            N--;
+            i--;
+        }
 
+        
+    }
+
+    printf("\n\nThe table after deleting '%d' : ", val);
     for(i=0;i<N;i++){
-        printf("%d | ", tab[i]);
+        printf(" %d | ", T[i]);
     }
-    printf("\n");
-    int n;
-    printf("\nVeillez entrez la valeur a supprimer : ");
-    scanf("%d", &n);
-     
-    for(i=0;i<N;i++)
-{
-    if(tab[i] == n){
-      p = 1;
-      for(j=i;j<N-1;j++){
-        tab[j] = tab[j+1];
 
-      }
-      N--;
-      i--;
-    }
+    return 0;
 
 }
 
-if(p==1){
-    printf("\nLa nouveau vecteur apres avoir supprimer %d est : ",n);
-    for(i=0;i<N;i++){
-        printf("%d | ", tab[i]);
+int exo7(){
+
+    int i,j;
+    double diff = 0;
+    double Gdiff = 0;
+    
+
+    double T[10];
+
+    for(i=0;i<10;i++){
+        printf("\nVeillez entrez la valeur de la case %d : ",i);
+        scanf("%lf", &T[i]);
     }
 
-}else{
-    printf("\n%d n'existe meme pas dans le vecteur", n);
-}
-return 0;
-}
 
+    printf("\nLe tableau : ");
+    for(i=0;i<10;i++){
+        printf(" %.2lf | ", T[i]);
+    }
+
+    for(i=0;i<10;i++){
+        for(j=i + 1;j<10;j++){
+            diff = T[j]-T[i];
+        }
+
+        if(diff<0){
+            diff = -diff;
+        }else if(diff>Gdiff){
+            Gdiff = diff;
+        }
+
+         
+    }
+
+    printf("\n\nThe greatest difference is : %.2lf", Gdiff);
+
+    return 0;
+}
 int main(){
-   //exo1();
-
-   //exo2();
-   exo6();
+   exo7();
 
    return 0;
-
 }
