fe3129ee220919c197a839c082f81fa95f988058
Yanis Rezgui <yanisrezgui28@gmail.com>
2024-12-27 23:56:04 +0100

    Add files via upload

--- exos.c
+++ exos.c
@@ -22,15 +22,69 @@ int Cube(int X){
     return c;
 }
 
+void Table2(int X){
+
+    int i;
+
+    printf("\nLa table de multiplication de %d est : ", X);
+    for(i=1;i<=10;i++){
+        printf("%d*%d = %d\n",X,i,X*i);
+    }
+
+}
+
+
+int Cube2(int X){
+    int c;
+    c=X*X*X;
+
+    return c;
+}
+
+
+bool annee(int X){
+
+    bool p = false;
+
+    if((X%4==0 && X%100!=0) || (X%4==0)){
+        p = true;
+    }
+
+    return p;
+}
+
+
+bool premier(int X){
+    int i;
+    bool p = true;
+
+    if(X == 1){
+        p = false;
+    }
+
+    i=2;
+    while(i<=X/2 && p==true){
+        if(X%i == 0)
+           {
+            p=false;
+           }
+    }
+
+    return p;
+}
+
+int ch(int x){
+
+    int nbr=0;
+    while(x>0){
+        x = x/10;
+        nbr++;
+    }
+
+    return nbr;
+}
+
 int main(){
-    int n;
 
-    printf("\nVeillez entrez un entier n : ");
-        scanf("%d", &n);
- 
-     printf("\nLe cube de %d est : %d ",n, Cube(n));
-
-
-    
-    return 0;
+printf("\nYou are beautiful");
 }
