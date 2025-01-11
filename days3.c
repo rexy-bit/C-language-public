8f8bbfc31e9fcd318c639011262dfd4db4d53b62
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-01-11 23:46:55 +0100

    Add files via upload

--- days3.c
+++ days3.c
@@ -1,44 +1,13 @@
 #include <stdio.h>
-main(){
-	int i;
-	
-	printf("Enter a number between 1 and seven : ");
-	scanf("%d", &i);
-	
-	switch(i){
-		case 1 :
-			printf("Sunday");
-			break;
-			
-		case 2 :
-			printf("Monday");
-			break;
-			
-		case 3 :
-			printf("Tuesday");
-			break;
-			
-		case 4 :
-			printf("Wendnesday");
-			break;
-			
-		case 5 :
-			printf("Thursday");
-			break;
-			
-		case 6 :
-			printf("Friday");
-			break;
-			
-		case 7 :
-			printf("Saturday");
-			break;
-			
-			default :
-				printf("Wrong number please enter a number between 1 and 7");
-				break;
-			
-	}
-	
-	return 0;
+int main()
+{
+    enum days {sun = 1, mon = 2, tue = 3, wed =4, thu = 5, fri = 6, sat = 7 };
+    int  today = 8;
+    if(today == 6 | today == 7 )
+    {
+       printf("its the weekend ! :)\n");
+    }else{
+            printf("I have to work to day :(\n ");
+         }
+    return 0;
 }
