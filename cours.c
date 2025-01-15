0eaecee39d6459d3ca5c8911f69c4f94dad7bd4c
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-01-15 23:40:37 +0100

    Add files via upload

--- cours.c
+++ cours.c
@@ -1,20 +1,48 @@
 #include <stdio.h>
 
+   
 void echange(int *x, int *y){
+
     int temp;
 
     temp = *x;
     *x = *y;
     *y = temp;
+
+}
+
+void echange2( int x, int y){
+    int temp;
+
+    temp = x;
+    x = y;
+    y = temp;
+
+    printf("\na = %d, b = %d", x, y);
+}
+
+void maximum(int *x, int *y, int *m){
+    if(*x < *y){
+        *m = *y;
+    }else{
+        *m = *x;
+    }
+    
 }
 int main(){
 
-    int a,b;
+     int a,b,max;
 
-    a=1,b=2;
-    printf("\nAvant l'echange a = %d, b = %d\n", a, b);
-    echange(&a,&b);
-    printf("\nApres l'echange a = %d, b = %d\n", a, b);
+     printf("\nVeillez entrez la valeur de a : ");
+     scanf("%d", &a);
+
+     printf("\nVeillez entrez la valeur de b : ");
+     scanf("%d", &b);
+
+     maximum(&a,&b,&max);
+     printf("\nLe maximum est : %d", max);
+
+
 
      return 0;
 }
