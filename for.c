5cddb794e1c9cf403a85fc409df577daf186d560
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-01-18 23:15:48 +0100

    Add files via upload

--- for.c
+++ for.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 int main(){
-	
-	
-	for(int i = 0; i <= 10; i++){
-		printf("%d", i);
-	}
-	
-	return 0;
+    
+    int i;
+    for(i=0; i<=10; i++){
+        printf("%d\n",i);
+    }
 
+   return 0;
 }
