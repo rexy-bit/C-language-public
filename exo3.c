032bd01087d8f8e967b6cd51c34a26331b8e5179
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-01-19 23:32:52 +0100

    Add files via upload

--- exo3.c
+++ exo3.c
@@ -1,20 +1,61 @@
 #include <stdio.h>
-#include <math.h>
+
 int main(){
 
-    int n, i;
-    float Un;
+    int n, min,max,cpt,s,op;
+    float moy;
 
-    printf("\nEntrez le dernier terme de la suite : ");
+    printf("\nVeillez entrez un nombre n : ");
     scanf("%d", &n);
 
-    Un = 1;
+    min = n;
+    max = n;
+    cpt = 0;
+    moy = 0;
+    s = 0;
 
-    for(i = 1;i <= n;i++){
-        Un = 2*sqrt(Un + 1) - 1;
+
+    while(n != 0){
+
+        cpt++;
+        s = s + n;
+        
+        if(n<min){
+            min = n;
+        }else if(max>n){
+            max = n;
+
+        }
+        printf("\nVEillez entrez un nombre n : ");
+        scanf("%d", &n);
+
     }
 
-    printf("\nLe dernier terme de la suite Un est / %.2f", Un);
+    moy = s/cpt;
+
+    printf("\nEntrez un 1 pour afficher le minimum : ");
+    printf("\nEntrez un 2 pour afficher le maximum : ");
+    printf("\nENtrez un 3 pour afficher la moyenne : \n");
+    printf("\n");
+    scanf("%d", &op);
+
+    switch(op){
+        case 1 : printf("\nLe minimum est : %d", min);
+        break;
+
+        case 2 : printf("\nLe maximum est : %d", max);
+        break;
+
+        case 3 :  printf("\nLa moyenne est : %.2f", moy);
+        break;
+
+
+        default : 
+          printf("\nLe nombre n'est pas 1 ou 2 ou 3 : veillez resaisir un autre nombre");
+          break;
+
+    }
 
     return 0;
+
 }
