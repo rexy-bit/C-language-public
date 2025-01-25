8bf2c3644ada3e55d1800e35bce4a79d8ba89786
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-01-26 00:18:51 +0100

    Add files via upload

--- exo.c
+++ exo.c
@@ -40,9 +40,65 @@ void exo2(){
         printf("\nLes deux chaine n'on pas la meme taile");
    }
 }
+
+void exo11(){
+
+    char c[20];
+
+    printf("\nVeillez entrez votre nom et prenom : ");
+    gets(c);
+    printf("\nVotre nom complet est : ");
+    puts(c);
+
+    
+}
+
+void exo22(){
+
+    char c1[20],c2[20];
+    int i;
+  int d = 0;
+
+    printf("\nVeillez entrez le premier mot : ");
+    gets(c1);
+    printf("\nVeillez entrez le deuxieme mot : ");
+    gets(c2);
+  
+    if(strlen(c1)==strlen(c2)){
+        for(i=0;i<strlen(c1);i++){
+         if(c1[i]!=c2[i]){
+            d++;
+         }
+        }
+        printf("\nLa distance de haming entre %s et %s est : %d", c1, c2, d);
+    }else{
+        printf("\nLes deux mot %s et %s n'ont meme pas le meme nombre de caracteres",c1 ,c2);
+    }
+
+}
+
+int exo3(char c[20]){
+
+    int i = 0;
+   
+    
+    while(c[i]!='\0'){
+       i++;
+    }
+
+    printf("\nLa longuer de votre chaine est : %d", i);
+
+    return(i);
+}
+
 int main(){
 
-  exo2();
+    char c1[20];
+
+    printf("\nVeillez entrez votre chaine : ");
+    gets(c1);
+
+    printf("\nLa longeur de votre chaine est : %d", exo3(c1));
 
     return 0;
 }
