2c05300b7ea6050314d45e9b3846596c526988b9
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-01-26 23:56:05 +0100

    Add files via upload

--- exemplemoy.c
+++ exemplemoy.c
@@ -2,27 +2,7 @@
 
 int main(){
 
-    int notes[5];
-    int S,i,n;
-    float moy;
-    
-
-    printf("\nVeillez Entrez le nombre d'etudiant de votre groupe : ");
-    scanf("%d", &n);
-
-     
-     S = 0;
-    for(i=1;i<=n;i++){
-        printf("\nEntrez la note %d de l'etudiant : ", i);
-        scanf("%d", &notes[i]);
-        
-        S = S + notes[i];
-    }
-
-
-    moy = S/n;
-
-    printf("\nLa moyenne de l'etudiant est : %.2f", moy);
+   printf("\nI'm in a stressed period of exems I'm sorry I can't code today I need sleep");
 
     return 0;
 }
