dc592774a4ccf452056428d24df6036520d88a4f
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-01-30 23:42:45 +0100

    Add files via upload

--- exocour.c
+++ exocour.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(){
 
-
-    char c[10];
-
-   printf("\nVeillez entrez la chaine de c1 : ");
-   gets( c );
-    char a[10];
-
+    int T[6];
     int i;
-    for(i=0;c[i]!='\0';i++){
-        a[i]=c[i];
-    }
-    a[i]='\0';
-    puts(a);
+    int S;
+    int *p;
+    p = T;
+   S = 0;
+
+   for(p = T; p < T + 6; p++){
+    printf("\nVeillez entrez la valeur de %d : ", p - T);
+    scanf("%d", p);
+   }
+   printf("\nVoici votre tableau : \n");
+   for(p = T;p<T+6;p++){
+    printf(" %d | ", *p);
+   }
+   
+   printf("\nCalculons la somme des elements du tableau : \n");
+   for(p = T;p<T+6;p++){
+    S += *p;
+   }
+
+   printf("\nLa somme est : %d", S);
     
+
+    return 0;
 }
