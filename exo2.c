e1106403c2ee1d307b91a28dd65e8d11ca1b8b80
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-04-03 23:52:04 +0100

    Add files via upload

--- exo2.c
+++ exo2.c
@@ -1,16 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+#include "exo2.h"
+
 
 int main(){
 
-    long long c = 2426555645;
+    temp t1,t2,t3;
 
-    int i = 1;
+    printf("\nRemplissage des informations : ");
+    printf("\nTemp 1 : ");
+    printf("\nVeillez entrez les infos de t1 : ");
+    do{
+        printf("\nHeures : ");
+        scanf("%d", &t1.h);
+    }while(t1.h<0 && t1.h > 59);
+    do{
+        printf("\nMinutes : ");
+        scanf("%d", &t1.m);
+    }while(t1.m < 0 && t1.m > 50);
+    do{
+        printf("\nSecondes : ");
+        scanf("%d", &t1.s);
+    }while(t1.s< 0 && t1.s > 59);
 
-    while(i*2 < c){
-        i *= 2;
-    }
+    printf("\nInformations du deuxieme temps : ");
+    do{
+        printf("\nHeures : ");
+        scanf("%d", &t2.h);
+    }while(t2.h<0 && t2.h > 59);
+    do{
+        printf("\nMinutes : ");
+        scanf("%d", &t2.m);
+    }while(t2.m < 0 && t2.m > 59);
+    do{
+        printf("\nSecondes : ");
+        scanf("%d", &t2.s);
+    }while(t2.s< 0 && t2.s > 59);
 
-    printf("\nThe largest power of two less than c is : %d", i);
+    t3 = diff(t1,t2);
+    printf("\nLa difference entre les deux temps est : ");
+    printf("\nHeures : %d", t3.h);
+    printf("\nMinutes : %d", t3.m);
+    printf("\nSecondes : %d", t3.s);
+
+
+
 
     return 0;
 }
