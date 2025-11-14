58902697a0a269bdd818d6f51bd9b0eeea813bc9
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-11-15 00:26:37 +0100

    Add files via upload

--- cour2.c
+++ cour2.c
@@ -1,18 +1,46 @@
-
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main(){
 
-    int T[4]={0,1,2,3};
 
-    int *p;
-    p = &T;
+    int N;
+    do{
+        printf("\nVeillez entrez la taille du tableau que vous voulez allouer dynamiquement : ");
+        scanf("%d", &N);
+    }while(N<0);
 
-    *(T+1)=11;
-    printf("%d\n", T[1]);
+    int *p = malloc(N*sizeof(int));
+
+    if(p == NULL){
+        printf("\nLa memoire n'est pas alloue.");
+        exit(0);
+    }else{
+
+        int S = 0;
+        float M;
+
+    for(int i = 0;i<N;i++){
+        printf("\nVeillez entrez l'element numero %d : ", i);
+        scanf("%d", p+i);
+        S = S + *(p+i);
+    }
+
+    printf("\nVoici votre tableau : \n");
+    for(int i = 0;i<N;i++){
+        printf("%d | ", *(p+i));
+    }
+
+    M = S/N;
+
+    printf("\nLa somme est : %d", S);
+    printf("\nLa moyenne est : %.2f", M);
+
     
-    
+}
 
+free(p);
 
-   return 0;
+return 0;
 }
